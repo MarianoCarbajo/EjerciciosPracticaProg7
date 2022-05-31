fc29f0591cde9325d66dbdb6f8f8bcc214b30a42
Mariano Carbajo <104107401+MarianoCarbajo@users.noreply.github.com>
2022-05-31 11:36:47 -0300

    Ejercicio 2 Finalización del segundo ejercicio

--- src/ejercicio2/index.ts
+++ src/ejercicio2/index.ts
@@ -15,7 +15,7 @@ let comisionA: number[] = [8, 5, 7, 10, 9, 2, 4, 8, 6, 8];
 let comisionB: number[] = [3, 6, 8, 5, 4, 7, 7, 4, 6, 8];
 let comisionC: number[] = [7, 6, 9, 8, 9, 8, 5, 7, 10, 9];
 let indice: number;
-//Función que devuelve el promedio del alumno
+//Función que devuelve el promedio de la comision
 function promedioNota(notas: number[]): number {
   let suma: number = 0;
   let promedio: number = notas.length;
@@ -44,4 +44,60 @@ function mayorPromedio(
   }
   return texto;
 }
+//Función que devuelve el menor promedio
+function menorPromedio(
+  comisionA: number[],
+  comisionB: number[],
+  comisionC: number[]
+): string {
+  let promedioA: number = promedioNota(comisionA);
+  let promedioB: number = promedioNota(comisionB);
+  let promedioC: number = promedioNota(comisionC);
+  let texto: string = "";
+  if (promedioA < promedioB && promedioA < promedioC) {
+    texto =
+      "El menor promedio es: " + promedioA + " y corresponde a la comisión A";
+  } else if (promedioB < promedioA && promedioB < promedioC) {
+    texto =
+      "El menor promedio es: " + promedioB + " y corresponde a la comisión B";
+  } else {
+    texto =
+      "El menor promedio es: " + promedioC + " y corresponde a la comisión C";
+  }
+  return texto;
+}
+//Función que muestra cantidad de desaprobados
+function totalDesaprobados(notas: number[]): number {
+  let cantidadDesaprobados: number = 0;
+  for (indice = 0; indice < notas.length; indice++) {
+    if (notas[indice] < 6) {
+      cantidadDesaprobados++;
+    }
+  }
+  return cantidadDesaprobados;
+}
+//Función que devuelve la menor nota de una comisión
+function menorNota(notas: number[]): number {
+  let menor: number = 10;
+  for (indice = 0; indice < notas.length; indice++) {
+    if (notas[indice] < menor) {
+      menor = notas[indice];
+    }
+  }
+  return menor;
+}
+
 console.log(mayorPromedio(comisionA, comisionB, comisionC));
+console.log(menorPromedio(comisionA, comisionB, comisionC));
+console.log(
+  "La comisión A tiene " + totalDesaprobados(comisionA) + " desaprobados"
+);
+console.log(
+  "La comisión B tiene " + totalDesaprobados(comisionB) + " desaprobados"
+);
+console.log(
+  "La comisión C tiene " + totalDesaprobados(comisionC) + " desaprobados"
+);
+console.log("La menor nota de la comisón A es: " + menorNota(comisionA));
+console.log("La menor nota de la comisón A es: " + menorNota(comisionB));
+console.log("La menor nota de la comisón A es: " + menorNota(comisionC));
